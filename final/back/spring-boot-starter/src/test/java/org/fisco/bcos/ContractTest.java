62e7614cc402c3f86e4df6bf366ed3b59528ffda
HawkDukez <747036607@qq.com>
2019-06-30 10:54:00 +0800

    sol转换成了java

--- final/back/spring-boot-starter/src/test/java/org/fisco/bcos/ContractTest.java
+++ final/back/spring-boot-starter/src/test/java/org/fisco/bcos/ContractTest.java
@@ -2,6 +2,7 @@ package org.fisco.bcos;
 
 import static org.junit.Assert.assertTrue;
 
+import org.fisco.bcos.Contracts.Market;
 import org.fisco.bcos.constants.GasConstants;
 import org.fisco.bcos.temp.HelloWorld;
 import org.fisco.bcos.web3j.crypto.Credentials;
@@ -9,6 +10,10 @@ import org.fisco.bcos.web3j.protocol.Web3j;
 import org.fisco.bcos.web3j.tx.gas.StaticGasProvider;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 
 public class ContractTest extends BaseTest {
 
@@ -18,21 +23,41 @@ public class ContractTest extends BaseTest {
     @Test
     public void deployAndCallHelloWorld() throws Exception {
         // deploy contract
-        HelloWorld helloWorld =
-                HelloWorld.deploy(
-                                web3j,
-                                credentials,
-                                new StaticGasProvider(
-                                        GasConstants.GAS_PRICE, GasConstants.GAS_LIMIT))
-                        .send();
-        if (helloWorld != null) {
-            System.out.println("HelloWorld address is: " + helloWorld.getContractAddress());
-            // call set function
-            helloWorld.set("Hello, World!").send();
-            // call get function
-            String result = helloWorld.get().send();
-            System.out.println(result);
-            assertTrue("Hello, World!".equals(result));
+//        HelloWorld helloWorld =
+//                HelloWorld.deploy(
+//                                web3j,
+//                                credentials,
+//                                new StaticGasProvider(
+//                                        GasConstants.GAS_PRICE, GasConstants.GAS_LIMIT))
+//                        .send();
+//        if (helloWorld != null) {
+//            System.out.println("HelloWorld address is: " + helloWorld.getContractAddress());
+//            // call set function
+//            helloWorld.set("Hello, World!").send();
+//            // call get function
+//            String result = helloWorld.get().send();
+//            System.out.println(result);
+//            assertTrue("Hello, World!".equals(result));
+//        }
+
+        try {
+            Market market = Market.deploy(
+                    web3j,
+                    credentials,
+                    new StaticGasProvider(
+                            GasConstants.GAS_PRICE, GasConstants.GAS_LIMIT)).send();
+
+            if (market != null) {
+                System.out.println("HelloWorld address is: " + market.getContractAddress());
+            }
         }
+        catch (Exception e){
+            final Writer result = new StringWriter();
+            final PrintWriter print = new PrintWriter(result);
+            e.printStackTrace(print);
+            System.out.println("==============================="+result.toString());
+
+        }
+
     }
 }

4a8ab36374b2d1580e4387c430a5dc13b7de8eca
HawkDukez <747036607@qq.com>
2019-07-01 10:57:38 +0800

    注册功能代码实现，未测试

--- final/back/spring-boot-starter/src/main/java/org/fisco/bcos/Application.java
+++ final/back/spring-boot-starter/src/main/java/org/fisco/bcos/Application.java
@@ -25,26 +25,6 @@ public class Application {
 
     public static void main(String[] args) throws Exception{
         SpringApplication.run(Application.class, args);
-        try {
-            Market market = Market.deploy(
-                    web3j,
-                    credentials,
-                    new StaticGasProvider(
-                            GasConstants.GAS_PRICE, GasConstants.GAS_LIMIT)).send();
-
-            if (market != null) {
-                System.out.println("HelloWorld address is: " + market.getContractAddress());
-            }
-        }
-        catch (Exception e){
-            final Writer result = new StringWriter();
-            final PrintWriter print = new PrintWriter(result);
-            e.printStackTrace(print);
-            System.out.println("==============================="+result.toString());
-
-        }
-
-
 
     }
 }
--- final/back/spring-boot-starter/src/main/java/org/fisco/bcos/Service/Impl/RegisterService.java
+++ final/back/spring-boot-starter/src/main/java/org/fisco/bcos/Service/Impl/RegisterService.java
@@ -1,15 +1,23 @@
 package org.fisco.bcos.Service.Impl;
 
 import com.alibaba.fastjson.JSONObject;
+import org.fisco.bcos.Contracts.Market;
 import org.fisco.bcos.Service.Interface.IRegisterService;
+import org.fisco.bcos.constants.GasConstants;
 import org.fisco.bcos.web3j.crypto.Credentials;
 import org.fisco.bcos.web3j.crypto.EncryptType;
 import org.fisco.bcos.web3j.crypto.gm.GenCredential;
+import org.fisco.bcos.web3j.protocol.Web3j;
+import org.fisco.bcos.web3j.tx.gas.StaticGasProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService implements IRegisterService {
 
+    @Autowired
+
+    private Web3j web3j;
     @Override
     public JSONObject register() {
         JSONObject object = new JSONObject();
@@ -20,6 +28,17 @@ public class RegisterService implements IRegisterService {
         String address = credentials.getAddress();
         String privateKey = credentials.getEcKeyPair().getPrivateKey().toString(16);
         String publicKey = credentials.getEcKeyPair().getPublicKey().toString(16);
+
+        String contract = "16fc777b4401962f5398f82342af29e18427c077";
+        Market market = Market.load(
+                contract,
+                web3j,
+                credentials,
+                new StaticGasProvider(
+                        GasConstants.GAS_PRICE, GasConstants.GAS_LIMIT));
+
+
+        market.createUser();
 
         object.put("checked",true);
         object.put("key",privateKey);
